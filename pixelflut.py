550d1b920d2e757a559485d20afb56d43485c986
Spühler Stefan <git@tuxli.ch>
2019-12-28 19:30:26 +0100

    reformat code

--- pixelflut.py
+++ pixelflut.py
@@ -5,20 +5,20 @@ from PIL import Image
 sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 sock.connect((sys.argv[1], int(sys.argv[2])))
 
-def pixel(x,y,r,g,b,a=255):
-  if a == 255:
-    sock.send(b'PX %d %d %02x%02x%02x\n' % (x,y,r,g,b))
-  else:
-    sock.send(b'PX %d %d %02x%02x%02x%02x\n' % (x,y,r,g,b,a))
+def pixel(x, y, r, g, b, a=255):
+    if a == 255:
+        sock.send(b'PX %d %d %02x%02x%02x\n' % (x, y, r, g, b))
+    else:
+        sock.send(b'PX %d %d %02x%02x%02x%02x\n' % (x, y, r, g, b, a))
 
 im = Image.open(sys.argv[3]).convert('RGBA')
-_,_,w,h = im.getbbox()
+_, _, w, h = im.getbbox()
 payloads = []
 for x in range(w):
-  for y in range(h):
-    r,g,b,a = im.getpixel((x,y))
-    payloads.append([x,y,r,g,b,a])
+    for y in range(h):
+        r, g, b, a = im.getpixel((x, y))
+        payloads.append([x, y, r, g, b, a])
 
 while True:
     for payload in payloads:
-        pixel(payload[0],payload[1],payload[2],payload[3],payload[4],payload[5])
+        pixel(payload[0], payload[1], payload[2], payload[3], payload[4], payload[5])
