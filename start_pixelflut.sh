86c67555ffc48b39ccc28bec30ebf0dc4fe2770c
Spühler Stefan <git@tuxli.ch>
2019-12-28 18:03:19 +0100

    update path

--- start_pixelflut.sh
+++ start_pixelflut.sh
@@ -1,5 +1,5 @@
-pkill -f pixelflut.py
+pkill -f pixelflut
 for (( c=0; c<=19; c++ ))
 do  
- python3 pixelflut.py 2001:67c:20a1:1111:2051:5dff:feda:2983 1234 $1 &
+ python3 /home/stefan/pixelflut/pixelflut.py 2001:67c:20a1:1111:2051:5dff:feda:2983 1234 $1 &
 done
