184414b4297deb6bd166bed3e394002b12ab6b29
Spühler Stefan <git@tuxli.ch>
2019-12-28 19:06:14 +0100

    add nohup

--- start_pixelflut.sh
+++ start_pixelflut.sh
@@ -1,4 +1,4 @@
 for (( c=0; c<=50; c++ ))
 do  
- python3 /home/stefan/pixelflut/pixelflut.py 2001:67c:20a1:1111:2051:5dff:feda:2983 1234 $1 &
+ nohup python3 /home/stefan/pixelflut/pixelflut.py 2001:67c:20a1:1111:2051:5dff:feda:2983 1234 $1 &
 done
