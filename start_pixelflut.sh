b483d6cb05bfbdd1d75c94157c1006a9440de5cc
Spühler Stefan <git@tuxli.ch>
2019-12-28 23:56:01 +0100

    update ip

--- start_pixelflut.sh
+++ start_pixelflut.sh
@@ -1,4 +1,4 @@
 for (( c=0; c<=50; c++ ))
 do  
- nohup python3 /home/stefan/pixelflut/pixelflut.py 151.217.111.34 1234 $1 800 0 &
+ nohup python3 /home/stefan/pixelflut/pixelflut.py 151.217.176.193 1234 $1 800 0 &
 done
