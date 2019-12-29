6342a5383556927963da877673c6adb7e6a13a5a
Spühler Stefan <git@tuxli.ch>
2019-12-29 01:54:41 +0100

    restart on crash

--- start_pixelflut.sh
+++ start_pixelflut.sh
@@ -1,4 +1,13 @@
-for (( c=0; c<=50; c++ ))
-do  
- nohup python3 /home/stefan/pixelflut/pixelflut.py 151.217.176.193 1234 $1 800 400 &
+while true
+do
+process_count=$(ps aux | grep pixelflut | wc -l)
+if [[ process_count -lt 10 ]]
+then
+	for (( c=0; c<=50; c++ ))
+	do  
+	 python3 /home/stefan/pixelflut/pixelflut.py 151.217.176.193 1234 $1 400 400 &
+	done
+fi
+sleep 10
 done
+
